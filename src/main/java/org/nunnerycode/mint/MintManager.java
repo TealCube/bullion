1d17ca8a34bcc21a4edc2ba7adf5abdcd4baa415
UltraFaceguy <kattreinkyle@gmail.com>
2019-03-25 12:40:45 -0400

    formatting

--- src/main/java/org/nunnerycode/mint/MintManager.java
+++ src/main/java/org/nunnerycode/mint/MintManager.java
@@ -1,24 +1,20 @@
 /**
- * The MIT License
- * Copyright (c) 2015 Teal Cube Games
+ * The MIT License Copyright (c) 2015 Teal Cube Games
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package org.nunnerycode.mint;
 
@@ -43,100 +39,100 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class MintManager {
 
-    private final MintPlugin plugin;
-    private final Map<UUID, PlayerAccount> playerAccountMap;
-    private final Map<UUID, BankAccount> bankAccountMap;
+  private final MintPlugin plugin;
+  private final Map<UUID, PlayerAccount> playerAccountMap;
+  private final Map<UUID, BankAccount> bankAccountMap;
 
-    public MintManager(MintPlugin mintPlugin) {
-        this.plugin = mintPlugin;
-        playerAccountMap = new ConcurrentHashMap<>();
-        bankAccountMap = new ConcurrentHashMap<>();
-    }
+  public MintManager(MintPlugin mintPlugin) {
+    this.plugin = mintPlugin;
+    playerAccountMap = new ConcurrentHashMap<>();
+    bankAccountMap = new ConcurrentHashMap<>();
+  }
 
-    public double getPlayerBalance(UUID uuid) {
-        Validate.notNull(uuid, "uuid cannot be null");
-        if (!playerAccountMap.containsKey(uuid)) {
-            playerAccountMap.put(uuid, new PlayerAccount(uuid));
-        }
-        return playerAccountMap.get(uuid).getBalance();
+  public double getPlayerBalance(UUID uuid) {
+    Validate.notNull(uuid, "uuid cannot be null");
+    if (!playerAccountMap.containsKey(uuid)) {
+      playerAccountMap.put(uuid, new PlayerAccount(uuid));
     }
+    return playerAccountMap.get(uuid).getBalance();
+  }
 
-    public void setPlayerBalance(UUID uuid, double balance) {
-        Validate.notNull(uuid, "uuid cannot be null");
-        PlayerAccount account =
-                playerAccountMap.containsKey(uuid) ? playerAccountMap.get(uuid) : new PlayerAccount(uuid);
-        account.setBalance(balance);
-        playerAccountMap.put(uuid, account);
-    }
+  public void setPlayerBalance(UUID uuid, double balance) {
+    Validate.notNull(uuid, "uuid cannot be null");
+    PlayerAccount account =
+        playerAccountMap.containsKey(uuid) ? playerAccountMap.get(uuid) : new PlayerAccount(uuid);
+    account.setBalance(balance);
+    playerAccountMap.put(uuid, account);
+  }
 
-    public double getBankBalance(UUID uuid) {
-        Validate.notNull(uuid, "uuid cannot be null");
-        if (!bankAccountMap.containsKey(uuid)) {
-            bankAccountMap.put(uuid, new BankAccount(uuid));
-        }
-        return bankAccountMap.get(uuid).getBalance();
+  public double getBankBalance(UUID uuid) {
+    Validate.notNull(uuid, "uuid cannot be null");
+    if (!bankAccountMap.containsKey(uuid)) {
+      bankAccountMap.put(uuid, new BankAccount(uuid));
     }
+    return bankAccountMap.get(uuid).getBalance();
+  }
 
-    public void setBankBalance(UUID uuid, double balance) {
-        Validate.notNull(uuid, "uuid cannot be null");
-        BankAccount account =
-                bankAccountMap.containsKey(uuid) ? bankAccountMap.get(uuid) : new BankAccount(uuid);
-        account.setBalance(balance);
-        bankAccountMap.put(uuid, account);
-    }
+  public void setBankBalance(UUID uuid, double balance) {
+    Validate.notNull(uuid, "uuid cannot be null");
+    BankAccount account =
+        bankAccountMap.containsKey(uuid) ? bankAccountMap.get(uuid) : new BankAccount(uuid);
+    account.setBalance(balance);
+    bankAccountMap.put(uuid, account);
+  }
 
-    public boolean hasPlayerAccount(UUID uuid) {
-        Validate.notNull(uuid, "uuid cannot be null");
-        return playerAccountMap.containsKey(uuid);
-    }
+  public boolean hasPlayerAccount(UUID uuid) {
+    Validate.notNull(uuid, "uuid cannot be null");
+    return playerAccountMap.containsKey(uuid);
+  }
 
-    public boolean hasBankAccount(UUID uuid) {
-        Validate.notNull(uuid, "uuid cannot be null");
-        return bankAccountMap.containsKey(uuid);
-    }
+  public boolean hasBankAccount(UUID uuid) {
+    Validate.notNull(uuid, "uuid cannot be null");
+    return bankAccountMap.containsKey(uuid);
+  }
 
-    public void removeBankAccount(UUID uuid) {
-        Validate.notNull(uuid);
-        bankAccountMap.remove(uuid);
-    }
+  public void removeBankAccount(UUID uuid) {
+    Validate.notNull(uuid);
+    bankAccountMap.remove(uuid);
+  }
 
-    public List<String> banksAsStrings() {
-        List<String> strings = new ArrayList<>();
-        for (UUID uuid : bankAccountMap.keySet()) {
-            strings.add(uuid.toString());
-        }
-        return strings;
+  public List<String> banksAsStrings() {
+    List<String> strings = new ArrayList<>();
+    for (UUID uuid : bankAccountMap.keySet()) {
+      strings.add(uuid.toString());
     }
+    return strings;
+  }
 
-    public Set<PlayerAccount> getPlayerAccounts() {
-        return new HashSet<>(playerAccountMap.values());
-    }
+  public Set<PlayerAccount> getPlayerAccounts() {
+    return new HashSet<>(playerAccountMap.values());
+  }
 
-    public Set<BankAccount> getBankAccounts() {
-        return new HashSet<>(bankAccountMap.values());
-    }
+  public Set<BankAccount> getBankAccounts() {
+    return new HashSet<>(bankAccountMap.values());
+  }
 
-    public void updateWallet(Player player) {
-        PlayerInventory pi = player.getInventory();
-        HiltItemStack wallet = new HiltItemStack(Material.PAPER);
-        double b = plugin.getEconomy().getBalance(player);
-        String walletName = TextUtils.color(plugin.getSettings().getString("config.wallet.name", ""));
-        wallet.setName(walletName);
-        wallet.setLore(TextUtils.args(
-            TextUtils.color(plugin.getSettings().getStringList("config.wallet.lore")),
-            new String[][]{{"%amount%", MintListener.DF.format(b)},
-                {"%currency%", b == 1.00D ? plugin.getEconomy().currencyNameSingular()
-                    : plugin.getEconomy().currencyNamePlural()}}));
-        if (pi.getItem(17) == null || pi.getItem(17).getType() != Material.AIR) {
-            pi.setItem(17, wallet);
-        } else if ( pi.getItem(17).getItemMeta().getDisplayName().equals(walletName)) {
-            pi.setItem(17, wallet);
-        } else {
-            ItemStack old = new HiltItemStack(pi.getItem(17));
-            pi.setItem(17, wallet);
-            pi.addItem(old);
-        }
-        player.updateInventory();
+  public void updateWallet(Player player) {
+    PlayerInventory pi = player.getInventory();
+    HiltItemStack wallet = new HiltItemStack(Material.PAPER);
+    double b = plugin.getEconomy().getBalance(player);
+    String walletName = TextUtils.color(plugin.getSettings().getString("config.wallet.name", ""));
+    wallet.setName(walletName);
+    wallet.setLore(TextUtils.args(
+        TextUtils.color(plugin.getSettings().getStringList("config.wallet.lore")),
+        new String[][]{{"%amount%", MintListener.DF.format(b)},
+            {"%currency%", b == 1.00D ? plugin.getEconomy().currencyNameSingular()
+                : plugin.getEconomy().currencyNamePlural()}}));
+    if (pi.getItem(17) == null || pi.getItem(17).getType() != Material.AIR) {
+      pi.setItem(17, wallet);
+    } else if (pi.getItem(17).getItemMeta().getDisplayName().equals(walletName)) {
+      pi.setItem(17, wallet);
+    } else {
+      ItemStack old = new HiltItemStack(pi.getItem(17));
+      pi.setItem(17, wallet);
+      pi.addItem(old);
     }
+    player.updateInventory();
+  }
 
 }
